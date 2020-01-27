c5ee9375c495291d55c7988e8a4aff09270a5bcf
Diego Serpa <diego.neves@serpro.gov.br>
2020-01-27 17:42:43 -0300

    Subindo arquivos esquecidos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { DespesasListComponent } from './despesas-list/despesas-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DespesasListComponent
+    DespesasListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/despesas-list/despesas.service.ts
+++ src/app/despesas-list/despesas.service.ts
@@ -1,16 +1,34 @@
 export class DespesasService {
+  despesas = [
+    { id: 1, descricao: "Despesa 1", data: "2019-11-20 09:41:00", valor: 12.8 },
+    { id: 2, descricao: "Despesa 2", data: "2019-11-21 10:30:00", valor: 10.0 },
+    { id: 1, descricao: "Despesa 1", data: "2019-11-20 09:41:00", valor: 12.8 },
+    { id: 2, descricao: "Despesa 2", data: "2019-11-21 10:30:00", valor: 10.0 }
+  ];
 
-     despesas = [
-        {id: 1, descricao: 'Despesa 1', data: '2019-11-20 09:41:00', valor: 12.80},
-        {id: 2, descricao: 'Despesa 2', data: '2019-11-21 10:30:00', valor: 10.00},
-        {id: 1, descricao: 'Despesa 1', data: '2019-11-20 09:41:00', valor: 12.80},
-        {id: 2, descricao: 'Despesa 2', data: '2019-11-21 10:30:00', valor: 10.00}];
+  constructor() {}
 
-    constructor(){}
+  getDespesas() {
+    console.log(this.despesas);
+    return this.despesas;
+  }
 
-    getDespesas(){
-        console.log(this.despesas);
-        return this.despesas;
-    }
-
+  // result(){
+  //     this.http.get(this.backEnd + '/api/despesas-list')
+  //     .pipe(
+  //         map(responseData => {
+  //             const array = [];
+  //             for (const key in responseData){
+  //             if(responseData.hasOwnProperty(key)){
+  //                 array.push({...responseData[key]});
+  //             }
+  //             }
+  //             return array;
+  //         })
+  //     ).subscribe(array => {
+  //         console.log("Executando o serviço: " + array);
+  //         this.despesas = array;
+  //     });
+  //     this.despesas;
+  // }
 }
