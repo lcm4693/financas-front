a711a113b820290cb61681cc7b4999b005412f13
Diego Serpa <diego.neves@serpro.gov.br>
2020-02-07 18:48:09 -0300

    Roteamento das páginas no angular com eventEmitter

--- src/app/despesa-new/despesa-new.component.ts
+++ src/app/despesa-new/despesa-new.component.ts
@@ -1,6 +1,6 @@
 import { DespesasService } from './../despesas-list/despesas.service';
 import { Despesa } from './../shared/despesa';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-despesa-new',
@@ -12,16 +12,27 @@ export class DespesaNewComponent implements OnInit {
   @Input()
   despesa: Despesa = new Despesa();
 
+  @Output()
+  featureSelected = new EventEmitter<string>();
+
   constructor(private despesasService: DespesasService) {}
 
   ngOnInit() {}
 
-  limparDespesaTela(){
+  limparDespesaTela() {
     this.despesa = new Despesa();
   }
 
-  salvarDespesa() {
-    this.despesasService.incluirDespesa(this.despesa);
+  changeScreen(tela: string) {
+    this.featureSelected.emit(tela);
+  }
+
+  async salvarDespesa() {
+    const dep: Despesa = await this.despesasService.incluirDespesa(this.despesa);
+    if (!dep) {
+      return;
+    }
     this.limparDespesaTela();
+    this.changeScreen('lista');
   }
 }
